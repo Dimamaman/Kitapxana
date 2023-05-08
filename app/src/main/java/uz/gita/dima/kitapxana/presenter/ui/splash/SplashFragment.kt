c44e9b6aab9c4824f3aaf4deb8cf41508bb11c8c
dinmuxammedmamanov <dimamanov37@gmail.com>
2023-05-09 01:16:53 +0500

    status bar color changed in splash fragment,then colo default

--- app/src/main/java/uz/gita/dima/kitapxana/presenter/ui/splash/SplashFragment.kt
+++ app/src/main/java/uz/gita/dima/kitapxana/presenter/ui/splash/SplashFragment.kt
@@ -1,18 +1,38 @@
 package uz.gita.dima.kitapxana.presenter.ui.splash
 
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import uz.gita.dima.kitapxana.R
 
 class SplashFragment: Fragment(R.layout.splash_fragment) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val window: Window = requireActivity().window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.splashStatusBarColor)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Handler(Looper.getMainLooper()).postDelayed({
             findNavController().navigate(R.id.action_splashFragment_to_mainFragment2)
         },2000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val window: Window = requireActivity().window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.gray)
     }
 }
